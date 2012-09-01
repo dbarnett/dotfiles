3d53657b6d4a306107eebdb4760533be1e0c7983
David Barnett <davidbarnett2@gmail.com>
2012-09-01 00:03:23 -0700

    Add right_prompt.sh

---
+++ right_prompt.sh
@@ -0,0 +1,24 @@
+function title {
+    case "$TERM" in
+    xterm*|rxvt*)
+        echo -en "\033]2;$1\007"
+        ;;
+    *)
+        ;;
+    esac
+}
+
+print_pre_prompt() {
+    PS1R=$(date)
+    PS1L_exp="${PS1L//\\u/$USER}"
+    PS1L_exp="${PS1L_exp//\\h/$HOSTNAME}"
+    SHORT_PWD=${PWD/$HOME/~}
+    PS1L_exp="${PS1L_exp//\\w/$SHORT_PWD}"
+    PS1L_clean="$(sed -r 's:\\\[([^\\]|\\[^]])*\\\]::g' <<<$PS1L_exp)"
+    PS1L_exp=${PS1L_exp//\\\[/}
+    PS1L_exp=${PS1L_exp//\\\]/}
+    PS1L_exp=$(eval echo '"'$PS1L_exp'"')
+    PS1L_clean=$(eval echo -e $PS1L_clean)
+    title $PS1L_clean
+    printf "%b%$(($COLUMNS-${#PS1L_clean}))b" "$PS1L_exp" "$PS1R"
+}
